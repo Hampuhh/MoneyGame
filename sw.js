b4e54a550f2f481b80efcb19f64a915ab3c786c8
Hampuhh <43082280+Hampuhh@users.noreply.github.com>
2026-02-18 23:34:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,27 @@
-const CACHE_NAME = 'money-game-v1';
-const urlsToCache = [
-  './',
-  './money-game.html',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap',
-  'https://agi-prod-file-upload-public-main-use1.s3.amazonaws.com/9ca668e1-5d2f-4870-976a-6da03921928e'
+const cacheName = 'money-game-v1';
+const appShellFiles = [
+  './index.html',
+  './icon.png',
+  './banner.png',
+  './fondo.png',
+  './manifest.json'
 ];
 
-// Instalar y cachear archivos
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
   );
 });
 
-// Interceptar peticiones y servir desde caché
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// Limpiar cachés antiguos
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request);
     })
   );
 });
